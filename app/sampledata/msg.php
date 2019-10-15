18c3f5e4d7ad106c77711203723e555bc2797d88
jaisalsabu <jaisal1010sabu@gmail.com>
2019-10-15 14:47:52 +0530

    Initial commit

---
+++ app/sampledata/msg.php
@@ -0,0 +1,56 @@
+<?php
+$Phone=$_POST["phone"];
+$Message=$_POST["message"];
+
+//Your authentication key
+$authKey = "298916AKBogUzOF1K85da56d38";
+
+//Sender ID,While using route4 sender id should be 6 characters long.
+$senderId = "102234";
+
+//Your message to send, Add URL encoding here.
+$message = urlencode(mysqli_query($con,$sql));
+
+//Define route 
+$route = "default";
+//Prepare you post parameters
+$postData = array(
+    'authkey' => $authKey,
+    'mobiles' => $Phone,
+    'message' => $message,
+    'sender' => $senderId,
+    'route' => $route
+);
+
+//API URL
+$url="http://api.msg91.com/api/sendhttp.php";
+
+// init the resource
+$ch = curl_init();
+curl_setopt_array($ch, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $postData
+    //,CURLOPT_FOLLOWLOCATION => true
+));
+
+
+//Ignore SSL certificate verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+
+//get response
+$output = curl_exec($ch);
+
+//Print error if any
+if(curl_errno($ch))
+{
+    echo 'error:' . curl_error($ch);
+}
+
+curl_close($ch);
+
+echo $output;
+?>
